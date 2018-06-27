d0431db50cd9b98c34083a10b691d42c621661d5
Anshuman Tiwari <at.slimshady@gmail.com>
2018-06-28 00:29:35 +0530

    Now user's can edit and delete only their post

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html',
+                  {'post': post, 'post_author': str(post.author), 'request_user': str(request.user)})
 
 
 @login_required
