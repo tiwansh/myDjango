3df8990a292b35ddea8270af397df19e4c4618cc
Anshuman Tiwari <at.slimshady@gmail.com>
2018-11-20 02:20:45 +0530

    Added tabs in my posts section using js

--- static/js/myLocation.js
+++ static/js/myLocation.js
@@ -58,3 +58,20 @@ function carousel() {
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
+function openSegment(evt, segment){
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        document.getElementById(segment).style.display = "block";
+        evt.currentTarget.className += " active";
+}
