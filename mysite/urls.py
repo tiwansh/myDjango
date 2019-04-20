4eb1cf9fbcddc19968a472cc2e884531ae4720a5
Anshuman Tiwari <at.slimshady@gmail.com>
2019-04-20 22:08:12 +0530

    Added local fileupload functionality for tinymce editor | Updated requirements

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-import blog.urls
+from filebrowser.sites import site
 
 urlpatterns = [
     # Examples:
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^admin/filebrowser/', include(site.urls)),
 ]
 
 if settings.DEBUG:
